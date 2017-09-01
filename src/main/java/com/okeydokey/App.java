381c2537f0406722cd0a20ddaa55641c650f7166
okeydokey <yunkim28@gmail.com>
2017-09-01 17:10:12 +0900

    JPQL 적용

--- src/main/java/com/okeydokey/App.java
+++ src/main/java/com/okeydokey/App.java
@@ -7,6 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @EnableAutoConfiguration
 @ComponentScan
@@ -22,10 +25,14 @@ public class App implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
+        customerRepository.save(new Customer(null, "Eidetoshi", "Aekisugi"));
+        customerRepository.save(new Customer(null, "Aidetoshi", "Bekisugi"));
+        customerRepository.save(new Customer(null, "Cidetoshi", "Cekisugi"));
+        customerRepository.save(new Customer(null, "Aidetoshi", "Dekisugi"));
 
-        System.out.println(created + " is created!");
+        Pageable pageable = new PageRequest(0, 3);
+        Page<Customer> page = customerRepository.findAll(pageable);
 
-        customerRepository.findAll().forEach(System.out::println);
+        page.getContent().forEach(System.out::println);
     }
 }
